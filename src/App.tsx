a5b56bbb7d544de770131f1e31d493b912c1447f
Jeroen Bruinsma <jeroen.bruinsma@cavotec.com>
2024-07-12 10:47:35 +0200

    add i18n

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login.tsx";
 import Sites from "./pages/Sites.tsx";
+import UnitDetailsPage from "./pages/UnitDetailsPage.tsx";
+import './i18n/config';
 
 function App() {
   return (
     <>
+    
       <Routes>
         <Route path="/sites" Component={Sites} />
         <Route path="/sites/:id" Component={Sites} />
-        <Route path="/unit/:id" Component={App} />
+        <Route path="/unit/:id" Component={UnitDetailsPage} />
         <Route path="/login" Component={Login} />
         <Route path="/" Component={Login} />
       </Routes>
