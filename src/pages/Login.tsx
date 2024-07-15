9452761cd0e2e99c7e453e474ab0008ad855f4b6
Jeroen Bruinsma <jeroen.bruinsma@cavotec.com>
2024-07-15 16:09:28 +0200

    make login reponsive

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -5,7 +5,7 @@ import right from "../assets/login_side_right.png";
 
 export default function LoginPage() {
   return (
-    <div style={{ display: "flex", alignItems: "center" }}>
+    <div style={{ display: "flex", alignItems: "center",  justifyContent: "center", height: "100vh"}}>
       <ImageContainer image={left} />
       <LoginFormContainer />
       <ImageContainer image={right} />
