d0cd4bebe81fe412e195c6054274408387a6f5c9
Jeroen Bruinsma <jeroen.bruinsma@cavotec.com>
2024-07-19 09:04:32 +0200

    use dynamic tableRowElement

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,56 +1,27 @@
-import { FunctionComponent, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { FunctionComponent } from "react";
 import styles from "./Table.module.css";
-import axios from "axios";
-import { site } from "../../types/sites";
 import React from "react";
+import { site } from "../../types/sites";
 
 export type TableType = {
-  className?: string;
   TableRowElement: any
+  tableColumns: string[]
+  tableData: site[] | undefined
 };
 
 
-const Table: FunctionComponent<TableType> = ({ className = "", TableRowElement}) => {
-  const [sites, set_sites] = useState<undefined | site[] >(undefined)
-  const getSites = async () => {
-    try{
-      const url = `http://127.0.0.1:5000/sites`
-      const res = await axios.get(url)
-      if(res?.data){
-        set_sites(res?.data)
-      }else{
-        set_sites(undefined)
-      }
-    }
-    catch(err){
-      console.log("err",err)
-    }
-  }
-  useEffect(()=> {
-    getSites()
-  }, [])
-
-
-  const columns = [
-    "Site name",
-    "Operator",
-    "SLA status",
-    "Data consent",
-    "Site health"]
-
+const Table: FunctionComponent<TableType> = ({ TableRowElement,tableColumns,tableData }) => {
 return ( <>
     <table className={styles.table}>
      <thead className={styles.tableHead}>
       <tr className={styles.tableRow}>
-        {columns.map((c, i) => <th key={i} scope="col" className={styles.tableHeader}>{c}</th>) }
+        {tableColumns.map((c, i) => <th key={i} scope="col" className={styles.tableHeader}>{c}</th>) }
       </tr>
     </thead>
     <tbody className={styles.tableBody}>
-      {sites?.map((s) => React.createElement(TableRowElement, {siteName: s?.SiteLocation__c, operator: s["Account.Name"], SLA:"Active", DC:"Yes", siteHealth:"-"}) )}
+      {tableData?.map((s) => React.createElement(TableRowElement, {siteName: s?.SiteLocation__c, operator: s["Account.Name"], SLA:"Active", DC:"Yes", siteHealth:"-"}) )}
       </tbody>
       </table>
-      
       </>
     );
 };
--- src/pages/Sites.tsx
+++ src/pages/Sites.tsx
@@ -1,20 +1,50 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useEffect, useState } from "react";
 import TopHeader from "../components/TopHeader/TopHeader";
 import Table from "../components/Table/Table";
 import SubsectionHeader from "../components/SubsectionHeader/SubsectionHeader";
 import { useTranslation } from "react-i18next";
 import TableRow from "../components/Table/TableRow";
+import { site } from "../types/sites";
+import axios from "axios";
 
 
 const SitesPage: FunctionComponent = () => {
   const { t } = useTranslation()
+
+  const [sites, set_sites] = useState<undefined | site[] >(undefined)
+  const getSites = async () => {
+    try{
+      const url = `http://127.0.0.1:5000/sites`
+      const res = await axios.get(url)
+      if(res?.data){
+        set_sites(res?.data)
+      }else{
+        set_sites(undefined)
+      }
+    }
+    catch(err){
+      console.log("err",err)
+    }
+  }
+  useEffect(()=> {
+    getSites()
+  }, [])
+
+  const columns = [
+    "Site name",
+    "Operator",
+    "SLA status",
+    "Data consent",
+    "Site health"]
+
   return (
     <>
       <TopHeader showImage={true} />
       <div style={{width: "100%", display: "flex", alignItems: "center", justifyContent: "center", flexDirection:"column"}}>
         	<div style={{width: "90%", display: "flex", alignItems: "left", justifyContent: "center", flexDirection:"column"}}>
+            {/* @ts-ignore */}
             <SubsectionHeader title={t("siteOverview")} center/>
-            <Table TableRowElement={TableRow}/>
+            <Table TableRowElement={TableRow} tableColumns={columns} tableData={sites}/>
         </div>
       </div>
     </>
