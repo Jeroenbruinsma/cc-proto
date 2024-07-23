5be0e8c61e26f4fa1f2b23f72e138ee3eb34759f
Jeroen Bruinsma <jeroen.bruinsma@cavotec.com>
2024-07-23 14:20:56 +0200

    add localstorage  option to influence backendUrl

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,3 @@
-export const backendUrl = "http://kiv.cavotecmm.org:5000"
+const nz_api_ip = "http://kiv.cavotecmm.org:5000"
+export const backendUrl = localStorage.getItem('backend') ? localStorage.getItem('backend') : nz_api_ip
+if(backendUrl !== nz_api_ip) console.log("alternative backend url used", backendUrl)
