9dd0253937e74e99269a8d5e86f118e547721f9f
Jeroen Bruinsma <jeroen.bruinsma@cavotec.com>
2024-07-25 13:55:01 +0200

    fix ts issues on i18n translations

--- src/@types/i18next.d.ts
+++ src/@types/i18next.d.ts
@@ -1,7 +1,9 @@
-import resources from './resources';
+import { resources, defaultNS } from "./i18n";
+
 
 declare module 'i18next' {
   interface CustomTypeOptions {
+    defaultNS: typeof defaultNS;
     resources: typeof resources;
     // if you see an error like: "Argument of type 'DefaultTFuncReturn' is not assignable to parameter of type xyz"
     // set returnNull to false (and also in the i18next init options)
