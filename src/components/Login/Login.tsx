e616cb6c0cca85401e5fc98a789dd3432b65c9ac
Jeroen Bruinsma <jeroen.bruinsma@cavotec.com>
2024-09-25 15:38:01 +0200

    improve styles

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -11,22 +11,24 @@ export type LoginType = {
 const Login: FunctionComponent<LoginType> = ({ className = "" }) => {
   const navigate = useNavigate()
   return (
-    <div className={[styles.login, className].join(" ")}>
-        <img className={styles.logo} src="/cavotec_logo.png"/>
-      <h1 className={styles.cavotecConnectInsights}>
-        Cavotec Connect Insights
-      </h1>
-      <InputField labelText="Username" />
-      <InputField labelText="Password" />
-      <div className={styles.insightsLink}>
-        <div className={styles.link}>
-          <div className={styles.clickMe}>Reset password</div>
+    <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+      <img className={styles.logo} src="/cavotec_logo.png"/>
+        <div className={[styles.login, className].join(" ")}>
+        <h1 className={styles.cavotecConnectInsights}>
+          Cavotec Connect Insights
+        </h1>
+        <InputField labelText="Username" />
+        <InputField labelText="Password" />
+        <div className={styles.insightsLink}>
+          <div className={styles.link}>
+            <div className={styles.clickMe}>Reset password</div>
+          </div>
         </div>
-      </div>
-      <div className={styles.loginButton}>
-        <button className={styles.button}>
-          <div className={styles.buttontext} onClick={()=> navigate("/sites")}>Login</div>
-        </button>
+        <div className={styles.loginButton}>
+          <button className={styles.button}>
+            <div className={styles.buttontext} onClick={()=> navigate("/sites")}>Login</div>
+          </button>
+        </div>
       </div>
     </div>
   );
