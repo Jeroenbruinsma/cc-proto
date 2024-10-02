ee096859b72e990885a94654028806a64672bd73
Jeroen Bruinsma <jeroen.bruinsma@cavotec.com>
2024-10-02 09:19:12 +0200

    allow user to hide warnig messages

--- src/components/InfoBox/InfoBox.tsx
+++ src/components/InfoBox/InfoBox.tsx
@@ -1,4 +1,6 @@
 import { useTranslation } from "react-i18next"
+import QM from "./x.svg";
+import { useState } from "react";
 
 interface InfoBoxProps{
     type: "validationNavigation" | "sitesPageTesting" | "assetNotValidated" | "accountNotValidated"
@@ -6,6 +8,7 @@ interface InfoBoxProps{
 
 export default function InfoBox({ type }:InfoBoxProps
 ) {
+    const [hide, set_hide] = useState(false) 
     const {t} =  useTranslation()
     let warning =  undefined
     let explanation = undefined
@@ -31,8 +34,15 @@ export default function InfoBox({ type }:InfoBoxProps
         explanation = t("accountValidation.explanation") 
         backgroundColor = "#f5bf89"
     }
+  if(hide) return <></>  
   return (
-    <div style={{width: "100%", backgroundColor , display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center", borderRadius:"15px", margin: "10px 0px 10px 0px"}}>
+    <div style={{width: "100%", backgroundColor , display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center", borderRadius:"15px", 
+    margin: "10px 0px 10px 0px", padding: "10px 10px"}}>
+      <div style={{display: "flex", alignItems: "flex-end", flexDirection: "column",     width: "100%"}}>
+         {/* 
+        //@ts-ignore */}
+          <QM fill="black" width="25px" onClick={()=> set_hide(true)} />
+        </div>
         <h2 style={{ marginBottom: "0px"}} >{warning}</h2>
         {explanation ? <h4 style={{textAlign: "center"}}>{explanation}</h4> : null }
         {resolution ? <p style={{textAlign: "center"}}>{resolution}</p> : null}
