287b4289600b32f876fe9e627c753d349f8fb9e2
Jeroen Bruinsma <jeroen.bruinsma@cavotec.com>
2024-11-07 11:27:01 +0100

    add kpi's

--- src/components/SubsectionHeader/SubsectionHeader.tsx
+++ src/components/SubsectionHeader/SubsectionHeader.tsx
@@ -5,17 +5,63 @@ export type SubsectionHeaderType = {
   className?: string;
   title: string
   center?: boolean
+  since?: boolean
+  selectedOption?: number
+  set_selectedOption?: (x: number) => void
+  set_showOptionDropdown?: (x:boolean) => void
+  showOptionDropdown?: boolean
+  dropdownOptions?: string[]
 };
 
 const SubsectionHeader: FunctionComponent<SubsectionHeaderType> = ({
   className = "",
   title,
-  center =  false
+  center =  false,
+  showOptionDropdown,
+  set_selectedOption,
+  selectedOption,
+  set_showOptionDropdown,
+  dropdownOptions
 }) => {
+  // const [selectedPeriod, set_selectedPeriod] =  useState(0)
+  // const [showPeriodDropdown, set_showPeriodDropdown] =  useState(false)
+  // const {t} = useTranslation()
+  // const periodOptions = ["7D","30D","12M"] // make api call?
+  // const periodText = periodOptions.map(o => t(`kpi.period.${o}`))  
+
+  const handleOptionClick = (i:number ) => {
+    set_selectedOption && set_selectedOption(i)
+    set_showOptionDropdown && set_showOptionDropdown(false)
+  }
   return (
     <div className={[styles.subsectionHeader, className].join(" ")}>
       <div className={styles.titleplussince}>
         <b style={{textAlign: center ? "center": "left"}} className={styles.kpiStatistics}>{title}</b>
+        { set_showOptionDropdown ? 
+        <>
+        <div className={styles.sinceDropdown} onClick={() => set_showOptionDropdown && set_showOptionDropdown(!showOptionDropdown)} >
+            <div className={styles.sinceDropdownInner}>
+              <img
+                className={styles.frameChild}
+                loading="lazy"
+                alt=""
+                src="/polygon-11.svg"
+              />
+            </div>
+            {/* <div className={styles.months} onClick={() => set_showOptionDropdown && set_showOptionDropdown(true)}>{dropdownOptions && dropdownOptions[selectedOption || 0]}</div> */}
+            <div className={styles.months} >{dropdownOptions && dropdownOptions[selectedOption || 0]}</div>
+          </div>
+            { showOptionDropdown ?
+            <div className={styles.periodDropdown}>
+              <ul className={styles.perodDropdownList}>
+              {dropdownOptions?.map((pt, i) => <li onClick={()=> handleOptionClick(i)}>{pt}</li>)}
+              </ul>
+            </div>
+            : null
+            }
+          </>
+          : null
+          }
       </div>
       <div className={styles.lineframe}>
         <div className={styles.lineframeChild} />
---
+++ src/types/kpi.ts
@@ -0,0 +1,9 @@
+interface kpi {
+    calculated_at: Date
+    id:  number
+    kpi_name: string
+    kpi_period_name: "CUSTOM"
+    kpi_period_seconds: number
+    kpi_result: number
+    kpi_unit: "%"
+  }
