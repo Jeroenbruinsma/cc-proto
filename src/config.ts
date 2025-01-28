fd6cd18cba7822bab7fed6ed1264df22f38df854
Jeroen Bruinsma <jeroen.bruinsma@cavotec.com>
2025-01-28 19:47:52 +0100

    use port 8000

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-const nz_api_ip = "http://api.cavotecconnect.com:5000/v0"
+const nz_api_ip = "http://api.cavotecconnect.com:8000/v0"
 
 export const backendUrl = localStorage.getItem('backend') ? localStorage.getItem('backend') : nz_api_ip
 if(backendUrl !== nz_api_ip) console.log("alternative backend url used", backendUrl)
