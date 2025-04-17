077eb6b544345413187cd2d165b4fe79682e12ca
Jeroen Bruinsma <jeroen.bruinsma@cavotec.com>
2025-04-17 14:22:45 +0200

    use login component as indicator for token exp

--- src/components/LoginFormContainer/LoginFormContainer.tsx
+++ src/components/LoginFormContainer/LoginFormContainer.tsx
@@ -1,17 +1,24 @@
 import { FunctionComponent } from "react";
 import Login from "../Login/Login";
 import styles from "./LoginFormContainer.module.css";
+import { useTranslation } from "react-i18next";
 
 export type LoginFormContainerType = {
   className?: string;
+  sessionExpired?: boolean;
 };
 
 const LoginFormContainer: FunctionComponent<LoginFormContainerType> = ({
   className = "",
+  sessionExpired
 }) => {
+  const {t} = useTranslation()
   return (
     <div className={[styles.loginFormContainer, className].join(" ")}>
       <Login />
+      { sessionExpired ? <div className={styles.loginFormErrors}>
+        <p className={styles.loginFormErrorsText}> {t("login.session.expired")}</p>
+      </div> :null}
     </div>
   );
 };
