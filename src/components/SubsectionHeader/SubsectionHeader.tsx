3d252643f79685ef24b299f24b97ae0547f7a967
Jeroen Bruinsma <jeroen.bruinsma@cavotec.com>
2025-04-24 14:28:18 +0200

    improve the dropdown menu

--- src/components/SubsectionHeader/SubsectionHeader.tsx
+++ src/components/SubsectionHeader/SubsectionHeader.tsx
@@ -1,35 +1,35 @@
 import { FunctionComponent } from "react";
 import styles from "./SubsectionHeader.module.css";
-import Toggle from 'react-toggle'
-import ExportIcon from "./IconExport.svg"
-import CalendarIcon from "./IconCalendar.svg"
+import Toggle from "react-toggle";
+import ExportIcon from "./IconExport.svg";
+import CalendarIcon from "./IconCalendar.svg";
 import { Link } from "react-router-dom";
 
 export type SubsectionHeaderType = {
   className?: string;
-  title: string
-  center?: boolean
-  since?: boolean
-  selectedOption?: number
-  set_selectedOption?: (x: number) => void
-  set_showOptionDropdown?: (x:boolean) => void
-  showOptionDropdown?: boolean
-  set_showOptionToggle?: (x:boolean) => void
-  optionToggleName?: string
-  set_toggleChecked?: (x:boolean) => void
-  toggleChecked?: boolean
-  dropdownOptions?: string[]
-  button?: () => void 
-  buttonText?: string
-  onClick?: () => void
-  exportData?: string | undefined
-  setRange?: boolean
+  title: string;
+  center?: boolean;
+  since?: boolean;
+  selectedOption?: number;
+  set_selectedOption?: (x: number) => void;
+  set_showOptionDropdown?: (x: boolean) => void;
+  showOptionDropdown?: boolean;
+  set_showOptionToggle?: (x: boolean) => void;
+  optionToggleName?: string;
+  set_toggleChecked?: (x: boolean) => void;
+  toggleChecked?: boolean;
+  dropdownOptions?: string[];
+  button?: () => void;
+  buttonText?: string;
+  onClick?: () => void;
+  exportData?: string | undefined;
+  setRange?: boolean;
 };
 
 const SubsectionHeader: FunctionComponent<SubsectionHeaderType> = ({
   className = "",
   title,
-  center =  false,
+  center = false,
   showOptionDropdown,
   set_selectedOption,
   selectedOption,
@@ -42,82 +42,90 @@ const SubsectionHeader: FunctionComponent<SubsectionHeaderType> = ({
   buttonText,
   onClick,
   exportData,
-  setRange 
+  setRange,
 }) => {
   // const [selectedPeriod, set_selectedPeriod] =  useState(0)
   // const [showPeriodDropdown, set_showPeriodDropdown] =  useState(false)
   // const {t} = useTranslation()
   // const periodOptions = ["7D","30D","12M"] // make api call?
-  // const periodText = periodOptions.map(o => t(`kpi.period.${o}`))  
-  const handleOptionClick = (i:number ) => {
-    set_selectedOption && set_selectedOption(i)
-    set_showOptionDropdown && set_showOptionDropdown(false)
-  }
+  // const periodText = periodOptions.map(o => t(`kpi.period.${o}`))
+  const handleOptionClick = (i: number) => {
+    set_selectedOption && set_selectedOption(i);
+    set_showOptionDropdown && set_showOptionDropdown(false);
+  };
   return (
     <div className={[styles.subsectionHeader, className].join(" ")}>
       <div className={styles.titleplussince}>
-        <b style={{textAlign: center ? "center": "left"}} className={[styles.sh, onClick ? styles.shName : null ].join(" ")} onClick={onClick}>{title}</b>
-        { set_showOptionDropdown ? 
-        <>
-        <div className={styles.sinceDropdown} onClick={() => set_showOptionDropdown && set_showOptionDropdown(!showOptionDropdown)} >
-            <div className={styles.sinceDropdownInner}>
-              <img
-                className={styles.frameChild}
-                loading="lazy"
-                alt=""
-                src="/polygon-11.svg"
-              />
+        <b
+          style={{ textAlign: center ? "center" : "left" }}
+          className={[styles.sh, onClick ? styles.shName : null].join(" ")}
+          onClick={onClick}
+        >
+          {title}
+        </b>
+        {set_showOptionDropdown ? (
+          <div className={styles.periodDropdownParent}>
+            <div
+              className={styles.sinceDropdown}
+              onClick={() =>
+                set_showOptionDropdown &&
+                set_showOptionDropdown(!showOptionDropdown)
+              }
+            >
+              <div className={styles.sinceDropdownInner}>
+                <CalendarIcon />
+              </div>
+              <div className={styles.months}>
+                <p>{dropdownOptions && dropdownOptions[selectedOption || 0]}</p>
+              </div>
             </div>
-            {/* <div className={styles.months} onClick={() => set_showOptionDropdown && set_showOptionDropdown(true)}>{dropdownOptions && dropdownOptions[selectedOption || 0]}</div> */}
-            <div className={styles.months} >{dropdownOptions && dropdownOptions[selectedOption || 0]}</div>
+            {showOptionDropdown ? (
+              <div className={styles.periodDropdown}>
+                <ul>
+                  {dropdownOptions?.map((pt, i) => (
+                    <li key={i} onClick={() => handleOptionClick(i)}>
+                      {pt}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ) : null}
           </div>
-            { showOptionDropdown ?
-            <div className={styles.periodDropdown}>
-              <ul className={styles.perodDropdownList}>
-              {dropdownOptions?.map((pt, i) => <li onClick={()=> handleOptionClick(i)}>{pt}</li>)}
-              </ul>
-            </div>
-            : null
-            }
-          </>
-          : null
-          }
+        ) : null}
 
-          { optionToggleName  ?
-            <div className={styles.toggleHolder}>
-                <Toggle
-                  icons={false}
-                  onChange={() => set_toggleChecked && set_toggleChecked(!toggleChecked) } 
-                  checked={toggleChecked}  
-                />
-                <span className={styles.toggleTextHolder}>
-                  <p className={styles.toggleText}>{optionToggleName}</p>
-                </span>
-            </div>
-            : null
-            }
-            {button ? <span className={styles.btnTextHolder} onClick={button}>
-                  <p className={styles.btnText}>{buttonText}</p>
-                </span> :
-            null}
-            { exportData ? 
-            <>
+        {optionToggleName ? (
+          <div className={styles.toggleHolder}>
+            <Toggle
+              icons={false}
+              onChange={() =>
+                set_toggleChecked && set_toggleChecked(!toggleChecked)
+              }
+              checked={toggleChecked}
+            />
+            <span className={styles.toggleTextHolder}>
+              <p className={styles.toggleText}>{optionToggleName}</p>
+            </span>
+          </div>
+        ) : null}
+        {button ? (
+          <span className={styles.btnTextHolder} onClick={button}>
+            <p className={styles.btnText}>{buttonText}</p>
+          </span>
+        ) : null}
+        {exportData ? (
+          <>
             <Link to={exportData} download target="_self">
-            
-            <span className={styles.btnTextHolder}>
-                  <ExportIcon/>
-            </span> 
+              <span className={styles.btnTextHolder}>
+                <ExportIcon />
+              </span>
             </Link>
-                </>:
-                
-            null
-            }
-            { setRange ? <span className={styles.btnTextHolder} onClick={button}>
-                  <CalendarIcon/>
-                </span> :
-            null
-            }
-
+          </>
+        ) : null}
+        {setRange ? (
+          <span className={styles.btnTextHolder} onClick={button}>
+            <CalendarIcon />
+          </span>
+        ) : null}
       </div>
       <div className={styles.lineframe}>
         <div className={styles.lineframeChild} />
